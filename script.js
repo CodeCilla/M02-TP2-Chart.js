1baf8b44a2a95a3e2fd6379ccb85178e018113d2
CodeCilla <priscilla.renault72460@gmail.com>
2025-10-27 13:00:59 +0100

    feat : connect api coincap

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+const ctx = document.getElementById('myChart');
+const chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi'],
+    datasets: [
+      {
+        label: 'Nombre de visiteurs',
+        data: [12, 19, 3, 5, 2],
+        backgroundColor: '#2f8ea3',
+      },
+    ],
+  },
+});
+
+const token =
+  'a5e69ea677b418dab9c40723d9a9f0103b9737c071fce7ae037cb7bb81abb840';
+fetch('https://rest.coincap.io/v3/price/bysymbol/BTC%2CETH%2CADA', {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${token}`,
+  },
+})
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
