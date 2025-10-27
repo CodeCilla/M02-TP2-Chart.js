c558197f75952c90e3ad053806566581787a1cf8
CodeCilla <priscilla.renault72460@gmail.com>
2025-10-27 14:53:45 +0100

    connect to api

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const ctx = document.getElementById('myChart');
 const chart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi'],
+    labels: ['Bitcoin', 'Ethereum', 'Cardano'],
     datasets: [
       {
-        label: 'Nombre de visiteurs',
-        data: [12, 19, 3, 5, 2],
+        label: 'Price in USD',
+        data: [],
         backgroundColor: '#2f8ea3',
       },
     ],
@@ -15,13 +15,30 @@ const chart = new Chart(ctx, {
 
 const token =
   'a5e69ea677b418dab9c40723d9a9f0103b9737c071fce7ae037cb7bb81abb840';
-fetch('https://rest.coincap.io/v3/price/bysymbol/BTC%2CETH%2CADA', {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${token}`,
-  },
-})
-  .then((res) => res.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.error(err));
+const apiUrl = 'https://rest.coincap.io/v3/price/bysymbol/BTC%2CETH%2CADA';
+
+
+async function updateChartData() {
+  try {
+    const res = await fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const apiData = await res.json();
+
+    console.log(apiData);
+
+
+    const prices = Object.values(apiData.data).map((x) => parseFloat(x));
+    chart.data.datasets[0].data = prices;
+    console.log(prices);
+    chart.update();
+  } catch (err) {
+    console.error(err);
+  }
+}
+updateChartData();
+
